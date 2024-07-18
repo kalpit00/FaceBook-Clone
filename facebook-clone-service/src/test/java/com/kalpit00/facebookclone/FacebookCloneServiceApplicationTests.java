e87f1df5509202e95bbe79a558434c5f6e909c13
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-18 12:12:38 +0530

    initial commit

---
+++ facebook-clone-service/src/test/java/com/kalpit00/facebookclone/FacebookCloneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalpit00.facebookclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookCloneServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
