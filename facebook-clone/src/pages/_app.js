17a1bd8ce0c7503481a25cf8ca201521465d830a
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-08-01 02:48:33 +0530

    add Session Provider

--- facebook-clone/src/pages/_app.js
+++ facebook-clone/src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
+import store from "../public/src/app/store";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return <SessionProvider session={pageProps.session}></SessionProvider>;
 }
+
+export default MyApp;
