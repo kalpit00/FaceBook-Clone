7ee4416b58511d98bf60ccb6de4b7e91db2660c9
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-08-01 03:36:10 +0530

    Add controller layer

---
+++ facebook-clone-service/src/main/java/com/kalpit00/facebookclone/controller/PostController.java
@@ -0,0 +1,18 @@
+package com.kalpit00.facebookclone.controller;
+
+import com.kalpit00.facebookclone.service.PostService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/post")
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+}
