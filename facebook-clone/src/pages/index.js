2a428ce38d96b9232478dd764fa728cd8e65ba55
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-08-01 02:49:49 +0530

    add serverSide props to index.js

--- facebook-clone/src/pages/index.js
+++ facebook-clone/src/pages/index.js
@@ -1,8 +1,9 @@
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 
-export default function Home() {
+export default function Home({ session }) {
   return (
     <div>
       <Head>
@@ -15,4 +16,12 @@ export default function Home() {
       </main>
     </div>
   );
+}
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
 }
