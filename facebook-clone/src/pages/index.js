a7407087efa93400b3a1b2b6824cf958fb241ff4
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-08-01 02:55:08 +0530

    Add a Feed component

--- facebook-clone/src/pages/index.js
+++ facebook-clone/src/pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Login from "../../components/Login";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
+import Feed from "../../components/Feed";
 
 export default function Home({ session }) {
   if (!session) return <Login />;
@@ -15,6 +16,8 @@ export default function Home({ session }) {
       <main className="flex bg-gray-100">
         {/* Left Sidebar */}
         <Sidebar />
+        {/* Feed */}
+        <Feed />
       </main>
     </div>
   );
