3a12e7e77f787924c781448f7b50124536fe8da5
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-08-01 03:21:51 +0530

    Redux cleanup

--- facebook-clone/src/pages/_app.js
+++ facebook-clone/src/pages/_app.js
@@ -1,9 +1,16 @@
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
-import store from "../public/src/app/store";
+import store from "../../public/store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
-  return <SessionProvider session={pageProps.session}></SessionProvider>;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
