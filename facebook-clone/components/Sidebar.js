b32ec3cb2a308590b68814a5703caacf473492d7
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-08-01 02:42:11 +0530

    Sidebar implemented

--- facebook-clone/components/Sidebar.js
+++ facebook-clone/components/Sidebar.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { ImUsers } from "react-icons/im";
+import { MdGroups } from "react-icons/md";
+import { AiOutlineShop } from "react-icons/ai";
+import { MdOutlineOndemandVideo, MdOutlineExpandMore } from "react-icons/md";
+import { BsStopwatch } from "react-icons/bs";
+import Image from "next/image";
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className="hidden lg:inline-flex flex-col py-2 pl-2 max-w-xl lg:min-w-[320px]">
+      <div className="flex items-center space-x-2 py-3 pl-4  hover:bg-gray-200 rounded-l-xl cursor-pointer ">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/800px-2021_Facebook_icon.svg.png"
+          height={40}
+          width={40}
+          className="rounded-full cursor-pointer"
+        />
+        <p className="hidden sm:inline-flex font-medium">{}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
